f893eb9abc1aa876f497ec942324177dabb9f39e
kevalmehta99 <54802718+kevalmehta99@users.noreply.github.com>
2019-12-18 21:42:32 +0530

    change billing

--- deepBlue/billing/models.py
+++ deepBlue/billing/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class patientbill(models.Model):
+    name = models.CharField(max_length=100)
+    phno = models.CharField(max_length=10)
+
+#class doctorfee(models.Model):
+#    doctor = models.ForeignKey(doctor,on_delete=models.CASCADE)
+#    timepp = models.DecimalField(max_digits=10,decimal_places=1)
+#    name = models.CharField(max_length=100)
+#    phno = models.CharField(max_length=10)
+#    email_id = models.EmailField()
+#    doctor_charge = models.ForeignKey()
+
+class billingQueue(models.Model):
+    date_time = models.DateTimeField(default=datetime.datetime.now())
+    patient = models.ForeignKey(patient,on_delete=models.CASCADE)
+    #doctor = models.ForeignKey(doctor,on_delete=models.CASCADE)
+    predicted_time = models.DecimalField(max_digits=10,decimal_places=1)
+    actual_time = models.DecimalField(max_digits=10,decimal_places=1,null=True,blank=True,default=None) #time_in_q  time_out - time_in
+    #time_in = models.ForeignKey()
