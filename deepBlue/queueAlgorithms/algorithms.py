2cdf1d76d2ce05e532e34de1ba81a3c4f8245fba
Preetnagda <preetnagda34@gmail.com>
2019-12-18 21:58:30 +0530

    queueAlgo

--- deepBlue/queueAlgorithms/algorithms.py
+++ deepBlue/queueAlgorithms/algorithms.py
@@ -2,15 +2,13 @@ from registration import models as registration_models
 
 def getDoctor_PatientEstimatedTime(patient,doctor):
     timepp = doctor.timepp
-    q_details = registration_models.appointmentQueue.objects.filter(doctor_required=doctor)
+    q_details = registration_models.appointmentQueue.objects.filter(doctor_required=doctor,actual_time=None)
     patientAhead = 2
     estimatedTime = 10
-    
+
     return {"patientAhead":patientAhead,"estimatedTime":estimatedTime}
 
 def getDoctor_OverallEstimatedTime(doctor):
-
-
     return 10
 
 def calculateDoctorTimePerPatient(latest_Time,doctor):
